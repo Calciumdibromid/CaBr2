edf78044e532817a90386fd987b53905d213ac8c
6543 <6543@obermui.de>
2021-02-14 21:26:49 +0100

    first draft

--- src-tauri/src/load_save/error.rs
+++ src-tauri/src/load_save/error.rs
@@ -2,16 +2,19 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum LoadSaveError {
-  #[error("unknown file type")]
-  UnknownFileType,
+    #[error("unknown file type")]
+    UnknownFileType,
 
-  #[error("failed to load file: '{0}'")]
-  DeserializeError(String),
+    #[error("failed to load file: '{0}'")]
+    DeserializeError(String),
 
-  #[error("parsing json failed: '{0}'")]
-  JsonError(#[from] serde_json::Error),
-  #[error("io error: '{0}'")]
-  IOError(#[from] std::io::Error),
+    #[error("parsing json failed: '{0}'")]
+    JsonError(#[from] serde_json::Error),
+    #[error("io error: '{0}'")]
+    IOError(#[from] std::io::Error),
+
+    #[error("render document failed: '{0}'")]
+    RenderError(String),
 }
 
 pub type Result<T> = std::result::Result<T, LoadSaveError>;
--- src-tauri/src/load_save/pdf/mod.rs
+++ src-tauri/src/load_save/pdf/mod.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use wkhtmltopdf::*;
 
 use super::{
   error::{LoadSaveError, Result},
@@ -9,7 +10,35 @@ pub struct PDF;
 
 impl Saver for PDF {
   fn save_document(&self, _filename: PathBuf, _document: CaBr2Document) -> Result<()> {
-    log::error!("not yet implemented");
-    Err(LoadSaveError::UnknownFileType)
+    let title = _document.header.document_title.clone();
+    match render_doc(_document) {
+      Err(e) => Err(LoadSaveError::RenderError(e.to_string())),
+      Ok(html) => match PdfApplication::new() {
+        Err(e) => Err(LoadSaveError::RenderError(e.to_string())),
+        Ok(mut pdf_app) => {
+          match pdf_app
+            .builder()
+            .orientation(Orientation::Landscape)
+            .margin(Size::Millimeters(50))
+            .title(title.as_str())
+            .build_from_html(&html)
+          {
+            Err(e) => Err(LoadSaveError::RenderError(e.to_string())),
+            Ok(mut pdfout) => match pdfout.save(_filename) {
+              Err(e) => Err(LoadSaveError::IOError(e)),
+              Ok(_) => Ok(()),
+            },
+          }
+        }
+      },
+    }
   }
+}
+
+const HTML_TEST: &str = r#"<html><body><div>foo</div></body></html>"#;
+
+// render_doc get CaBr2Document and return html (dummy at the moment)
+pub fn render_doc(_document: CaBr2Document) -> Result<String> {
+  log::warn!("dummy values");
+  return Ok(HTML_TEST.to_string());
 }
