91fb1be698744c827dc841332ed526235c543ec7
6543 <6543@obermui.de>
2021-02-14 23:28:12 +0100

    Arc<Mutex<PDF-APP>>

--- src-tauri/src/load_save/pdf/mod.rs
+++ src-tauri/src/load_save/pdf/mod.rs
@@ -1,4 +1,6 @@
+use lazy_static::lazy_static;
 use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
 use wkhtmltopdf::*;
 
 use super::{
@@ -8,29 +10,33 @@ use super::{
 
 pub struct PDF;
 
+lazy_static! {
+  static ref pdf_app: Arc<Mutex<PdfApplication>> =
+    Arc::new(Mutex::new(PdfApplication::new().expect("cant initialice wkhtmltopdf")));
+}
+
 impl Saver for PDF {
   fn save_document(&self, _filename: PathBuf, _document: CaBr2Document) -> Result<()> {
     let title = _document.header.document_title.clone();
     match render_doc(_document) {
       Err(e) => Err(LoadSaveError::RenderError(e.to_string())),
-      Ok(html) => match PdfApplication::new() {
-        Err(e) => Err(LoadSaveError::RenderError(e.to_string())),
-        Ok(mut pdf_app) => {
-          match pdf_app
-            .builder()
-            .orientation(Orientation::Landscape)
-            .margin(Size::Millimeters(50))
-            .title(title.as_str())
-            .build_from_html(&html)
-          {
-            Err(e) => Err(LoadSaveError::RenderError(e.to_string())),
-            Ok(mut pdfout) => match pdfout.save(_filename) {
-              Err(e) => Err(LoadSaveError::IOError(e)),
-              Ok(_) => Ok(()),
-            },
-          }
+      Ok(html) => {
+        match pdf_app
+          .lock()
+          .unwrap()
+          .builder()
+          .orientation(Orientation::Landscape)
+          .margin(Size::Millimeters(50))
+          .title(title.as_str())
+          .build_from_html(&html)
+        {
+          Err(e) => Err(LoadSaveError::RenderError(e.to_string())),
+          Ok(mut pdfout) => match pdfout.save(_filename) {
+            Err(e) => Err(LoadSaveError::IOError(e)),
+            Ok(_) => Ok(()),
+          },
         }
-      },
+      }
     }
   }
 }
