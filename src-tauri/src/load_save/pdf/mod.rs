e7400d523c024cc3e5e6e5e1b18f41b97f48fbfd
6543 <6543@obermui.de>
2021-02-17 23:51:23 +0100

    Add Save to PDF (#120)
    * rust: add dep wkhtmltopdf:0.3.0
    
    * Arc<Mutex<PDF-APP>>
    
    * first test of pdf thread
    
    * bring functionality of previous version back
    
    * CI: add wkhtmltox + missing dependencys
    
    * fixed render_doc method signature
    
    * added handlebars
    
    * added lopdf as dependency
    
    * added html templates
    
    * implemented pdf saving
    
    - added rendering of templates
    - added merging of pages into one
    
    * CI: bump ubuntu image to 20.04
    
    * added wkhtmltox as info to README
    
    * added TODO for path
    
    Co-authored-by: crapStone <crapstone01@gmail.com>
    Co-authored-by: 6543 <6543@obermui.de>

--- src-tauri/src/load_save/pdf/mod.rs
+++ src-tauri/src/load_save/pdf/mod.rs
@@ -1,4 +1,19 @@
-use std::path::PathBuf;
+mod merge;
+
+use std::{
+  env,
+  fs::OpenOptions,
+  io::{BufReader, Read},
+  path::PathBuf,
+  sync::{mpsc, Arc, Mutex},
+  thread,
+};
+
+use handlebars::Handlebars;
+use lazy_static::lazy_static;
+use lopdf::Document;
+use serde::Serialize;
+use wkhtmltopdf::{Orientation, PageSize, PdfApplication, Size};
 
 use super::{
   error::{LoadSaveError, Result},
@@ -8,8 +23,153 @@ use super::{
 pub struct PDF;
 
 impl Saver for PDF {
-  fn save_document(&self, _filename: PathBuf, _document: CaBr2Document) -> Result<()> {
-    log::error!("not yet implemented");
-    Err(LoadSaveError::UnknownFileType)
+  fn save_document(&self, filename: PathBuf, document: CaBr2Document) -> Result<()> {
+    lazy_static! {
+      static ref PDF_THREAD_CHANNEL: Arc<Mutex<(mpsc::SyncSender<(String, String)>, mpsc::Receiver<Result<Vec<u8>>>)>> =
+        Arc::new(Mutex::new(init_pdf_application()));
+    }
+
+    let title = document.header.document_title.clone();
+    match render_doc(&document) {
+      Err(e) => Err(e),
+      Ok(pages) => {
+        let channels = PDF_THREAD_CHANNEL.lock().unwrap();
+
+        let mut pdfs = Vec::with_capacity(2);
+        for page in pages {
+          channels
+            .0
+            .send((page, title.clone()))
+            .expect("sending data to pdf thread failed");
+
+          let pdf: Vec<u8> = channels.1.recv().expect("receiving data from pdf thread failed")?;
+          pdfs.push(pdf);
+        }
+
+        let mut documents = Vec::with_capacity(pdfs.len());
+        for pdf in pdfs {
+          match Document::load_mem(&pdf) {
+            Ok(doc) => documents.push(doc),
+            Err(_) => return Err(LoadSaveError::PdfMergeError("loading pdf failed".into())),
+          }
+        }
+
+        let mut merged_pdf = merge::merge_pdfs(documents)?;
+        merged_pdf.save(filename)?;
+
+        Ok(())
+      }
+    }
   }
+}
+
+/// render_doc get CaBr2Document and return html (dummy at the moment)
+fn render_doc(document: &CaBr2Document) -> Result<Vec<String>> {
+  #[derive(Debug, Serialize)]
+  struct Context<'a> {
+    stylesheet: &'a String,
+    document: &'a CaBr2Document,
+  }
+
+  lazy_static! {
+    static ref REG: Arc<Mutex<Option<(String, Handlebars<'static>)>>> = Arc::new(Mutex::new(None));
+  }
+
+  let mut reg = REG.lock().unwrap();
+
+  if reg.is_none() {
+    *reg = Some(init_handlebars()?);
+  }
+
+  let reg = reg.as_ref().unwrap();
+  let context = Context {
+    stylesheet: &reg.0,
+    document,
+  };
+  Ok(vec![
+    reg.1.render("first", &context)?,
+    reg.1.render("second", &context)?,
+  ])
+}
+
+#[inline]
+fn init_handlebars() -> Result<(String, Handlebars<'static>)> {
+  let mut reg = Handlebars::new();
+  let program_path = PathBuf::from(env::args().next().unwrap());
+  // TODO make path os independent
+  let mut program_path = program_path.parent().unwrap().to_path_buf();
+  program_path.push("templates");
+  program_path.push("file");
+
+  for name in ["first", "second"].iter() {
+    let filename = program_path.with_file_name(name).with_extension("html");
+    log::debug!("template path: {:?}", filename);
+    reg.register_template_file(name, filename)?;
+  }
+
+  let mut buf = String::new();
+  let file = OpenOptions::new()
+    .read(true)
+    .open(program_path.with_file_name("styles").with_extension("css"))?;
+  let mut reader = BufReader::new(file);
+  reader.read_to_string(&mut buf)?;
+
+  Ok((buf, reg))
+}
+
+fn init_pdf_application() -> (mpsc::SyncSender<(String, String)>, mpsc::Receiver<Result<Vec<u8>>>) {
+  let (tauri_tx, pdf_rx) = mpsc::sync_channel(0);
+  let (pdf_tx, tauri_rx) = mpsc::sync_channel(0);
+
+  /* #region  pdf thread */
+
+  thread::spawn(move || {
+    log::debug!("[pdf_thread]: initializing pdf application");
+    let mut pdf_app = match PdfApplication::new() {
+      Ok(app) => app,
+      Err(e) => {
+        log::error!("[pdf_thread]: initialization of pdf application failed");
+        pdf_tx
+          .send(Err(LoadSaveError::PdfError(e)))
+          .expect("[pdf_thread]: pdf thread could not send data");
+        return;
+      }
+    };
+
+    loop {
+      log::trace!("[pdf_thread]: waiting for html to convert");
+      let (html, title) = pdf_rx.recv().expect("[pdf_thread]: pdf thread could not receive data");
+      log::trace!("[pdf_thread]: got html");
+
+      // needed for rust to resolve types
+      let title: String = title;
+
+      let mut buf = Vec::new();
+
+      let result = match pdf_app
+        .builder()
+        .page_size(PageSize::A4)
+        .orientation(Orientation::Portrait)
+        .margin(Size::Millimeters(15))
+        .title(&title)
+        .build_from_html(&html)
+      {
+        Ok(mut pdfout) => match pdfout.read_to_end(&mut buf) {
+          Ok(_) => Ok(buf),
+          Err(e) => Err(LoadSaveError::IOError(e)),
+        },
+        Err(e) => Err(LoadSaveError::PdfError(e)),
+      };
+
+      log::trace!("[pdf_thread]: sending result");
+      pdf_tx
+        .send(result)
+        .expect("[pdf_thread]: pdf thread could not send data");
+      log::trace!("[pdf_thread]: finished");
+    }
+  });
+
+  /* #endregion */
+
+  (tauri_tx, tauri_rx)
 }
