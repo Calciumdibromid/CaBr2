76489485cd039176044e9d4d41ee54092ff67f95
crapStone <crapstone01@gmail.com>
2021-02-18 02:12:48 +0100

    removed debug-helper script (#166)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import debugMain from './debug-helper';
-
 import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
@@ -7,9 +5,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 if (environment.production) {
   enableProdMode();
-}
-else {
-  debugMain();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
