e3037e62ab90d8a7d8fffe02c87ecc95f116a727
6543 <6543@obermui.de>
2021-03-08 02:29:01 +0100

    fix getStrings

--- src/app/@core/services/i18n/i18n.service.ts
+++ src/app/@core/services/i18n/i18n.service.ts
@@ -1,12 +1,8 @@
 import { strings as de } from '../../../../assets/strings.json';
 import { strings as en } from '../../../../assets/strings_en.json';
 
-export interface i18n {
-  getStrings(lang: string): any;
-}
-
 export class i18n {
-  getStrings(lang: string) {
+  static getStrings(lang: string): any {
     if (lang === 'de') {
       return de;
     }
