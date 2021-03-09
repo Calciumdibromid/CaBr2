7d4a4ff81ada3212a3ff3cd3a09e6c77305b7296
Epsilon02 <epsilon_02+github@mailbox.org>
2021-03-09 20:38:57 +0100

    localized strings

--- src/app/yes-no-dialog/yes-no-dialog.component.ts
+++ src/app/yes-no-dialog/yes-no-dialog.component.ts
@@ -1,12 +1,16 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+import { strings } from '../../assets/strings.json';
+
 @Component({
   selector: 'app-yes-no-dialog',
   templateUrl: './yes-no-dialog.component.html',
   styleUrls: ['./yes-no-dialog.component.scss'],
 })
 export class YesNoDialogComponent implements OnInit {
+  strings = strings;
+
   /**
    * To use this component you must inject `MatDialog`.
    * The value `iconName` is optional.
