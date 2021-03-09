38363a226291159bbff21059dcd0c1d93e1fa005
Epsilon02 <epsilon_02+github@mailbox.org>
2021-03-09 19:03:26 +0100

    adde jsdoc

--- src/app/yes-no-dialog/yes-no-dialog.component.ts
+++ src/app/yes-no-dialog/yes-no-dialog.component.ts
@@ -7,6 +7,23 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./yes-no-dialog.component.scss'],
 })
 export class YesNoDialogComponent implements OnInit {
+  /**
+   * To use this component you must inject `MatDialog`.
+   * The value `iconName` is optional.
+   * Get icon names from [here](https://material.io/resources/icons/?icon=info&style=baseline)
+   * ```ts
+   * matDialog.open(ManualComponent, {
+   *  data: {
+   *    iconName: 'exampleIcon',
+   *    title: 'Example title',
+   *    content: 'Example content'
+   *  }
+   * });
+   * ```
+   *
+   * @param dialogRef
+   * @param data
+   */
   constructor(
     public dialogRef: MatDialogRef<YesNoDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { iconName: string; title: string; content: string },
