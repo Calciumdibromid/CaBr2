863d178e1cf16fb6e8b23f931aa9f5f990c56a7a
crapStone <crapstone01@gmail.com>
2021-04-24 22:44:10 +0200

    migrated first plugin

--- src-tauri/cabr2_config/src/cmd.rs
+++ src-tauri/cabr2_config/src/cmd.rs
@@ -1,39 +1,42 @@
-use serde::Deserialize;
+use serde_json::Value;
 
-use super::types::FrontendConfig;
+use crate::{
+  error::Result,
+  handler,
+  types::{FrontendConfig, GHSSymbols, LocalizedStringsHeader},
+};
 
-#[derive(Debug, Deserialize)]
-#[serde(tag = "cmd", rename_all = "camelCase")]
-pub enum Cmd {
-  GetProgramVersion {
-    callback: String,
-    error: String,
-  },
-  GetConfig {
-    callback: String,
-    error: String,
-  },
-  SaveConfig {
-    config: FrontendConfig,
-    callback: String,
-    error: String,
-  },
-  GetHazardSymbols {
-    callback: String,
-    error: String,
-  },
-  GetAvailableLanguages {
-    callback: String,
-    error: String,
-  },
-  GetLocalizedStrings {
-    language: String,
-    callback: String,
-    error: String,
-  },
-  GetPromptHtml {
-    name: String,
-    callback: String,
-    error: String,
-  },
+#[tauri::command]
+pub fn get_program_version() -> &'static str {
+  env!("CARGO_PKG_VERSION")
+}
+
+#[tauri::command]
+pub fn get_config() -> Result<FrontendConfig> {
+  handler::get_config()
+}
+
+#[tauri::command]
+pub fn save_config(config: FrontendConfig) -> Result<()> {
+  handler::save_config(config)
+}
+
+#[tauri::command]
+pub fn get_hazard_symbols() -> Result<GHSSymbols> {
+  handler::get_hazard_symbols()
+}
+
+#[tauri::command]
+pub fn get_available_languages() -> Result<Vec<LocalizedStringsHeader>> {
+  handler::get_available_languages()
+}
+
+#[tauri::command]
+pub fn get_localized_strings(language: String) -> Result<Value> {
+  handler::get_localized_strings(language)
+}
+
+#[tauri::command]
+pub fn get_prompt_html(name: String) -> Result<String> {
+  handler::get_prompt_html(name)
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,18 +4,20 @@ fn main() {
   // must be initialized first
   let logger = cabr2_logger::Logger::new();
 
-  let config = cabr2_config::Config;
+  let config = cabr2_config::Config::default();
   let search = cabr2_search::Search::new();
   let load_save = cabr2_load_save::LoadSave::new(search.get_provider_mapping());
 
   log::debug!("initializing tauri application...");
 
-  tauri::AppBuilder::new()
+  tauri::Builder::new()
     .plugin(config)
     .plugin(load_save)
     .plugin(logger)
     .plugin(search)
-    .setup(|_, s| log::debug!("tauri setup complete ({})", s))
-    .build()
-    .run();
+    .setup(|_| {
+      log::debug!("tauri setup complete");
+      Ok(())
+    })
+    .run(tauri::generate_context!());
 }
