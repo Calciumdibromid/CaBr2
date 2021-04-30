9fc36336f5d3f74a5edc722421c10ab0b0a9ac73
6543 <6543@obermui.de>
2021-05-01 00:47:21 +0200

    make every file type a feature (#461)
    * make wkhtmltopdf dependency as option
    
    * added more features and files root Cargo.toml
    
    Co-authored-by: crapStone <crapstone01@gmail.com>

--- src-tauri/cabr2_load_save/src/error.rs
+++ src-tauri/cabr2_load_save/src/error.rs
@@ -6,21 +6,30 @@ pub enum LoadSaveError {
   #[error("unknown file type")]
   UnknownFileType,
 
+  #[cfg(feature = "beryllium")]
   #[error("failed to load file: '{0}'")]
   DeserializeError(String),
 
   #[error("file already exists: '{0}'")]
   FileExists(String),
 
+  #[cfg(feature = "pdf")]
   #[error("merging of pdfs failed: '{0}'")]
   PdfMergeError(String),
 
+  #[cfg(feature = "pdf")]
   #[error("creating template failed: '{0}'")]
   TemplateError(#[from] handlebars::TemplateFileError),
+
+  #[cfg(feature = "pdf")]
   #[error("rendering document failed: '{0}'")]
   RenderError(#[from] handlebars::RenderError),
+
+  #[cfg(feature = "pdf")]
   #[error("PDF creation failed: {0}")]
   PdfError(#[from] wkhtmltopdf::Error),
+
+  #[cfg(feature = "cabr2")]
   #[error("parsing json failed: '{0}'")]
   JsonError(#[from] serde_json::Error),
   #[error("io error: '{0}'")]
