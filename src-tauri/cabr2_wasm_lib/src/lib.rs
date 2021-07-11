f8fa30ca1530db51459db5ff53ba36afb18fb15a
crapStone <crapstone01@gmail.com>
2021-07-11 21:02:29 +0200

    added rust doc

--- src-tauri/cabr2_wasm_lib/src/lib.rs
+++ src-tauri/cabr2_wasm_lib/src/lib.rs
@@ -11,7 +11,7 @@ extern "C" {
   fn alert(s: &str);
 }
 
-/// must be run first
+/// Must be run first to initialize all things.
 #[wasm_bindgen]
 pub fn init() {
   #[cfg(feature = "debug_build")]
@@ -20,13 +20,25 @@ pub fn init() {
   cabr2_load_save::wasm::init(HashMap::new())
 }
 
+/// Converts a CaBr2Document into a binary array that can be saved by the client.
+///
+/// May throw errors.
 #[wasm_bindgen]
 pub fn save_document(file_type: String, document: String) -> Result<Vec<u8>> {
-  let document: CaBr2Document = serde_json::from_str(&document).unwrap();
-  let res = cabr2_load_save::wasm::save_document(file_type, document).unwrap();
-  Ok(res)
+  let document: CaBr2Document = match serde_json::from_str(&document) {
+    Ok(document) => document,
+    Err(err) => return Err(JsValue::from(err.to_string())),
+  };
+
+  match cabr2_load_save::wasm::save_document(file_type, document) {
+    Ok(res) => Ok(res),
+    Err(err) => Err(JsValue::from(err.to_string())),
+  }
 }
 
+/// Converts a binary array into a CaBr2Document.
+///
+/// May throw errors.
 #[wasm_bindgen]
 pub fn load_document(file_type: String, doc: Vec<u8>) -> Result<String> {
   let document = match cabr2_load_save::wasm::load_document(file_type, doc) {
@@ -40,8 +52,19 @@ pub fn load_document(file_type: String, doc: Vec<u8>) -> Result<String> {
   }
 }
 
+/// Returns all available document types.
+/// May has to be extended with types directly supported in JS.
+///
+/// May throw errors.
 #[wasm_bindgen]
 pub fn get_available_document_types() -> Result<String> {
-  let types = cabr2_load_save::wasm::get_available_document_types().unwrap();
-  Ok(serde_json::to_string(&types).unwrap())
+  let types = match cabr2_load_save::wasm::get_available_document_types() {
+    Ok(types) => types,
+    Err(err) => return Err(JsValue::from(err.to_string())),
+  };
+
+  match serde_json::to_string(&types) {
+    Ok(res) => Ok(res),
+    Err(err) => Err(JsValue::from(err.to_string())),
+  }
 }
