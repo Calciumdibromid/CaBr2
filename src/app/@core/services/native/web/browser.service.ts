3ff8175b08ca3841a66056d28626d16e8f4e24b4
Epsilon02 <epsilon_02+github@mailbox.org>
2021-07-11 21:50:20 +0200

    implemented browserservice

--- src/app/@core/services/native/web/browser.service.ts
+++ src/app/@core/services/native/web/browser.service.ts
@@ -1,23 +1,43 @@
-import { INativeService } from '../native.interface';
+import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// TODO use vars and remove this lines
+import { INativeService } from '../native.interface';
+import Logger from '../../../utils/logger';
+
+const logger = new Logger('service.browser');
+
+const logCall = (name: string) => logger.error('called inaccessible tauri service: [', name, ']');
 
 @Injectable()
 export class BrowserService implements INativeService {
-  openUrl(url: string, openWith?: string): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-  open(options?: any): Observable<string | string[]> {
-    throw new Error('Method not implemented.');
-  }
-  save(options?: any): Observable<string | string[]> {
-    throw new Error('Method not implemented.');
+  openUrl(url: string, _?: string): Promise<void> {
+    window.open(url, '_blank');
+    return Promise.resolve();
   }
 
-  promisified<T>(cmd: string, args?: any): Observable<T> {
-    throw new Error('Method not implemented.');
+  open(_?: any): Observable<string | string[]> {
+    const inputField = new HTMLInputElement();
+    inputField.style.display = 'none';
+    document.append(inputField);
+
+    return new Observable((sub) => {
+      inputField.addEventListener(
+        'change',
+        (event: any) => {
+          sub.next(event.target.files[0]);
+        },
+        { once: true },
+      );
+      inputField.click();
+    });
+  }
+
+  save(options?: any): Observable<string | string[]> {
+    return of(options.filter ?? 'cb2');
+  }
+
+  promisified<T>(_cmd: string, _args?: any): Observable<T> {
+    logCall('promisified');
+    return new Observable();
   }
 }
